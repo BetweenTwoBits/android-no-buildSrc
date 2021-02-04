89a76b3e5f30fed8977f35a3c8a770d155894f7d
Brandon Davis <brandon_davis5@comcast.com>
2021-02-04 13:47:58 -0700

    update kotlin again…

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     // TODO Does this file even need to exist?
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
     }
 }
 
