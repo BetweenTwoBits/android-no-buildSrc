51493fd5fffdb535ac5710b8e0d461bb6434088c
Brandon Davis <brandon.davis1@nbcuni.com>
2022-03-01 14:58:05 -0700

    move app gradle conf to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,37 @@
 plugins {
-    id("android-app")
+    id("com.android.application")
+    id("kotlin-android")
+    //id("spotless-conf")
 }
 
 android {
+    compileSdkVersion = 31
+
     defaultConfig {
         applicationId = "com.betweentwobits.app"
+        minSdkVersion = 21
+        targetSdkVersion = 31
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
 dependencies {
     implementation(project(":myLibrary"))
+    implementation(libs.android.material)
     implementation(libs.androidx.annotations)
+    implementation(libs.androidx.app.compat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.constraint.layout)
+    implementation(libs.kotlin.stdlib)
 }
