f1c46b9e6806538504e5c074a7a7ad678f9fec3b
Brandon Davis <brandon.davis1@nbcuni.com>
2022-03-01 14:58:15 -0700

    move library gradle conf to build.gradle

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,3 +1,32 @@
 plugins {
-    id("android-lib")
+    id("com.android.library")
+    id("kotlin-android")
+    //id("spotless-conf")
+}
+
+android {
+    compileSdkVersion = 31
+
+    defaultConfig {
+        minSdkVersion = 21
+        targetSdkVersion = 31
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.app.compat)
 }
